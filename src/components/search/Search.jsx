c818885bb8f183061bc854ef231a732c4276ba6a
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2023-02-24 01:34:04 +0300

    Firebase/firestore Search on user

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,20 +1,67 @@
+import { useState } from 'react';
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import { db } from '../../firebase';
 import './Search.scss';
 
 const Search = () => {
+  const [userName, setUserName] = useState('');
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(false);
+
+  const hadleSearch = async () => {
+    // Create a reference to the users collection
+    const usersRef = collection(db, 'users');
+
+    // Create a query against the collection.
+    const q = query(usersRef, where('displayName', '==', userName));
+
+    const querySnapshot = await getDocs(q);
+    try {
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        // console.log(doc.id, ' => ', doc.data());
+        setUser(doc.data());
+      });
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  const handleChange = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const handleEnterPress = (event) => {
+    if (event.code === 'Enter') {
+      hadleSearch();
+      setUserName('');
+    }
+  };
+
   return (
     <div className="search">
       <div className="search-form">
-        <input type="text" placeholder="Find a user" />
-      </div>
-      <div className="user-chat">
-        <img
-          src="https://www.updatedcelebrities.com/wp-content/uploads/2022/11/IMG_20221101_210132.jpg"
-          alt=""
+        <input
+          type="text"
+          placeholder="Find a user"
+          value={userName}
+          onChange={handleChange}
+          onKeyDown={handleEnterPress}
         />
-        <div className="user-chat_info">
-          <span>Miranda Cohen</span>
-        </div>
       </div>
+      {error && (
+        <span className="error-message">
+          Sothing went wrong, please try agin later
+        </span>
+      )}
+      {user && (
+        <div className="user-chat">
+          <img src={user.photoURL} alt={user.displayName} />
+          <div className="user-chat_info">
+            <span>{user.displayName}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
