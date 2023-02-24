c09b6c0bd8bdde8c43504eaf574a73178998d11d
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2023-02-24 03:19:03 +0300

    Fetch chat conversations

--- src/components/chats/Chats.jsx
+++ src/components/chats/Chats.jsx
@@ -1,40 +1,38 @@
+import { useState, useEffect, useContext } from 'react';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { db } from '../../firebase';
+import { authContext } from '../../context/authContext';
 import './Chats.scss';
 
 const Chats = () => {
-  return (
-    <div className="chats">
-      <div className="user-chat">
-        <img
-          src="https://www.updatedcelebrities.com/wp-content/uploads/2022/11/IMG_20221101_210132.jpg"
-          alt=""
-        />
+  const [chats, setChats] = useState([]);
+  const { currentUser } = useContext(authContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, 'userChats', currentUser.uid), (doc) => {
+        setChats(Object.entries(doc.data()));
+      });
+      return () => {
+        unsub();
+      };
+    };
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+
+  const renderChats = chats.map((chat) => {
+    return (
+      <div className="user-chat" key={chat[0]}>
+        <img src={chat[1].userInfo.photoURL} alt="" />
         <div className="user-chat_info">
-          <span>Miranda Cohen</span>
-          <p>Hello!</p>
+          <span>{chat[1].userInfo.displayName}</span>
+          <p>{chat[1].userInfo.lastMessage?.text}</p>
         </div>
       </div>
-      <div className="user-chat">
-        <img
-          src="https://phantom-marca.unidadeditorial.es/90249c1989b7939a526e7c9afb569708/resize/1320/f/webp/assets/multimedia/imagenes/2022/12/31/16725192936240.jpg"
-          alt=""
-        />
-        <div className="user-chat_info">
-          <span>Leo Messi</span>
-          <p>Hello!</p>
-        </div>
-      </div>
-      <div className="user-chat">
-        <img
-          src="https://static.wikia.nocookie.net/warner-bros-entertainment/images/3/33/Audrey_Hepburn.jpg"
-          alt=""
-        />
-        <div className="user-chat_info">
-          <span>Audrey Hepburn</span>
-          <p>Hello!</p>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  });
+
+  return <div className="chats">{renderChats}</div>;
 };
 
 export default Chats;
